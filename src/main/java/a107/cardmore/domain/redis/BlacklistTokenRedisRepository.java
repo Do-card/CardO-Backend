90a941c56614449446bb471cd1668cefb18bffc3
정한수 <jounghs3933@gmail.com>
2024-10-29 15:44:31 +0900

    feat: Redis Cluster 설정 추가

---
+++ src/main/java/a107/cardmore/domain/redis/BlacklistTokenRedisRepository.java
@@ -0,0 +1,32 @@
+package a107.cardmore.domain.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import static a107.cardmore.util.constant.RedisPrefix.BLACKLIST;
+
+@Slf4j
+@Component
+public class BlacklistTokenRedisRepository extends BaseRedisRepository<String> {
+
+    public BlacklistTokenRedisRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.prefix = BLACKLIST;
+        this.defaultTtl = 60 * 60 * 24 * 7; //7일
+    }
+
+    public void save(String token, long ttl){
+        save(token, "EXPIRED", ttl);
+    }
+
+    public boolean hasKey(String token){
+        Boolean hasKey = redisTemplate.hasKey(prefix + token);
+
+        if (hasKey == null){
+            throw new RuntimeException();
+        }
+        return hasKey;
+    }
+
+}
