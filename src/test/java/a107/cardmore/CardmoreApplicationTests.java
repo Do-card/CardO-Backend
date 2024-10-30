8f63bbc05dfdb3943a956f2ecad1a221b6c76e25
박진우 <namid0711@naver.com>
2024-10-30 15:27:39 +0900

    test: 기본적인 테스트 코드 작성 및 테스트 환경 설정

--- src/test/java/a107/cardmore/CardmoreApplicationTests.java
+++ src/test/java/a107/cardmore/CardmoreApplicationTests.java
@@ -1,10 +1,11 @@
 package a107.cardmore;
 
+import a107.cardmore.common.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CardmoreApplicationTests {
+class CardmoreApplicationTests extends BaseTest {
 
 	@Test
 	void contextLoads() {
