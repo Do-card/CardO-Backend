7a4fb1647c2d6be76d0b0986d36559bcc3d45ed4
dahyunko <kdhgby@naver.com>
2024-10-31 13:16:35 +0900

    feat: es setting added

--- src/main/java/a107/cardmore/domain/item/entity/ItemDocument.java
+++ src/main/java/a107/cardmore/domain/item/entity/ItemDocument.java
@@ -1,6 +1,5 @@
 package a107.cardmore.domain.item.entity;
 
-import jakarta.persistence.GeneratedValue;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -16,10 +15,31 @@ import static org.springframework.data.elasticsearch.annotations.FieldType.*;
 @Document(indexName = "item_index")
 public class ItemDocument {
     @Id
-    @GeneratedValue
     @Field(type = Keyword)
     private String id;
 
     @Field(type = Text)
-    private String title;
+    private String category;
+
+    @Field(type=Text)
+    private String poiId;
+
+    @Field(type = Long)
+    private long userId;
+
+    @Field(type = Long)
+    private long markerId;
+
+    @Field(type = Long)
+    private long itemId;
+
+    @Field(type = Text)
+    private String itemName;
+
+    @Field(type = Float)
+    private float latitude;
+
+    @Field(type = Float)
+    private float longitude;
+
 }
