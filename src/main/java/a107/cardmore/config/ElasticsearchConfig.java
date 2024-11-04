32d1b039ee5177097b89e04067db717d8fdc41b2
dahyunko <kdhgby@naver.com>
2024-11-04 12:32:54 +0900

    feat: aggretion not yet

--- src/main/java/a107/cardmore/config/ElasticsearchConfig.java
+++ src/main/java/a107/cardmore/config/ElasticsearchConfig.java
@@ -2,9 +2,11 @@ package a107.cardmore.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.config.EnableElasticsearchAuditing;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -12,7 +14,7 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configurable
 @EnableElasticsearchRepositories
 @EnableElasticsearchAuditing
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
+public class ElasticsearchConfig extends ElasticsearchConfiguration{
     @Value("${spring.data.elasticsearch.url}")
     private String url;
 
@@ -22,4 +24,5 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
                 .connectedTo(url)
                 .build();
     }
+
 }
--- src/main/java/a107/cardmore/domain/item/service/ItemElasticService.java
+++ src/main/java/a107/cardmore/domain/item/service/ItemElasticService.java
@@ -1,15 +1,54 @@
 package a107.cardmore.domain.item.service;
 
+import a107.cardmore.domain.item.entity.ItemDocument;
 import a107.cardmore.domain.item.repository.ItemElasticRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.AggregationContainer;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ItemElasticService {
     private final ItemElasticRepository itemElasticRepository;
+//    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    public void getTrends(){
+        IndexCoordinates index = IndexCoordinates.of("user_log");
+
+        Criteria criteria = new Criteria();
+        Query query = new CriteriaQuery(criteria);
+        /*
+        // 집계 추가
+        AggregationBuilder aggregation = AggregationContainer.terms("category_counts")
+                .field("category.keyword")  // .keyword 필드를 통해 정확히 일치하는 문자열을 기준으로 집계
+                .size(10);  // 상위 10개의 카테고리만 반환
+
+        // 쿼리에 집계 추가
+        query.addAggregation(aggregation);
+
+        // Elasticsearch로 쿼리 실행 및 집계 결과 가져오기
+        AggregationResults aggregationResults = operations.aggregate(query, IndexCoordinates.of("user_log"));
+
+        // 결과를 Map으로 변환
+        Map<String, Long> categoryCounts = new HashMap<>();
+        aggregationResults.forEach(bucket -> categoryCounts.put(bucket.getKeyAsString(), bucket.getDocCount()));
+
+
+        SearchHits<?> hits = operations.search(query, ItemDocument.class, index);
+        */
+    }
 
 /*
 @Transactional(readOnly = true)
