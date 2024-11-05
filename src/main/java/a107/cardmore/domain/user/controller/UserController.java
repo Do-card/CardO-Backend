20e4174ef87bcb65b9538af793047fad7fe1a53a
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-11-05 14:13:39 +0900

    feat: React Native 통신 API 구현

--- src/main/java/a107/cardmore/domain/user/controller/UserController.java
+++ src/main/java/a107/cardmore/domain/user/controller/UserController.java
@@ -1,8 +1,13 @@
 package a107.cardmore.domain.user.controller;
 
+import a107.cardmore.domain.fcm.service.FCMModuleService;
+import a107.cardmore.domain.fcm.service.FCMService;
+import a107.cardmore.domain.user.dto.FCMTokenRequestDto;
+import a107.cardmore.domain.user.dto.PositionRequestDto;
 import a107.cardmore.domain.user.service.UserService;
 import a107.cardmore.util.base.BaseSuccessResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
+    private final FCMModuleService fcmModuleService;
+    private final FCMService fcmService;
 
     @GetMapping
     public BaseSuccessResponse<String> userNickName(){
@@ -22,14 +30,19 @@ public class UserController {
     }
 
     @PostMapping("/position")
-    public BaseSuccessResponse<String> userPosition(){
+    public BaseSuccessResponse<String> userPosition(PositionRequestDto requestDto) {
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        return new BaseSuccessResponse<>(userService.userNickName(userEmail));
+
+        return new BaseSuccessResponse<>(userService.checkMarker(userEmail, requestDto));
     }
 
     @PostMapping("/token")
-    public BaseSuccessResponse<String> userToken(){
+    public BaseSuccessResponse<Void> saveToken(FCMTokenRequestDto requestDto){
+        log.info("Save token : {}", requestDto.getToken());
+
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        return new BaseSuccessResponse<>(userService.userNickName(userEmail));
+        userService.saveToken(userEmail, requestDto.getToken());
+
+        return new BaseSuccessResponse<>(null);
     }
 }
