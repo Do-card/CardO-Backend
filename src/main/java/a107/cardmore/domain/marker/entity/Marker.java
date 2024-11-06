857c1eabd0c94504a3c1f65de7f8d1907e1b6512
정한수 <jounghs3933@gmail.com>
2024-11-06 14:14:02 +0900

    hotfix: marker의 user eager fetch로 받아오도록 수정

--- src/main/java/a107/cardmore/domain/marker/entity/Marker.java
+++ src/main/java/a107/cardmore/domain/marker/entity/Marker.java
@@ -36,7 +36,7 @@ public class Marker extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
