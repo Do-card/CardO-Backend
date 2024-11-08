81ccbf96a8f03c75ebebc43ea6328428f994ed0f
정한수 <jounghs3933@gmail.com>
2024-11-08 16:58:35 +0900

    modify: FCM access, refresh 토큰 처리 방식 변경

--- src/main/java/a107/cardmore/domain/redis/RefreshTokenRedisRepository.java
+++ src/main/java/a107/cardmore/domain/redis/RefreshTokenRedisRepository.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import static a107.cardmore.util.constant.RedisPrefix.ACCESS_TOKEN;
 import static a107.cardmore.util.constant.RedisPrefix.REFRESH_TOKEN;
 
 @Slf4j
